6422566bdd5499261640041ba2f5a387088a6941
dondonco <dondon.co19@gmail.com>
2021-05-18 19:30:45 -0700

    Create Switcharoo.java

---
+++ edabit/java/medium/Switcharoo.java
@@ -0,0 +1,6 @@
+/*
+Create a function that takes a string and returns a new string with its first and last characters swapped, except under two conditions:
+
+1. If the length of the string is less than two, return "Incompatible.".
+2. If the first and last characters are the same, return "Two's a pair.".
+*/
