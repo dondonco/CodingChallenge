cf6a653fd7812fbbcf429a2bc8fe299a1c12d42b
dondonco <dondon.co19@gmail.com>
2021-05-18 19:43:33 -0700

    Update ReverseString.java

--- edabit/java/medium/ReverseString.java
+++ edabit/java/medium/ReverseString.java
@@ -1,9 +1,10 @@
+//Write a function that reverses a string. Make your function recursive.
+
 public class ReverseString {
-	public static String reverse(String str) 
-		String rev = "";
-		for(String s: str.split("")){
-			rev = s + rev;
+	public static String reverse(String str) {
+		if(!str.equals("")) {
+			return reverse(str.substring(1)) + str.charAt(0);
 		}
-		return rev;
+		return str;
 	}
 }
