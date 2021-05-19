da09760816f67488879a025b6fd3465a6e03f240
dondonco <dondon.co19@gmail.com>
2021-05-18 18:48:32 -0700

    Create PerfectSquarePatch.java

---
+++ edabit/java/medium/PerfectSquarePatch.java
@@ -0,0 +1,11 @@
+public class Challenge {
+	public static int[][] squarePatch(int n) {
+		int[][] result = new int[n][n];
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				result[i][j] = n;
+			}
+		}
+		return result;
+	}
+}
